e1d00af34a077906faddbb4c73731bc96002f311
John Doe <local@local.local>
2023-02-18 14:19:14 +0200

    Centering

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.scss'
 function App() {
   const redSquare = useRef()
   const blueSquare = useRef()
+  const elements = useRef()
   const [currColor, setCurrColor] = useState("")
 
   const shapeColor = (event: any) => {
@@ -23,25 +24,26 @@ function App() {
     const xPos = event.clientX
     const yPos = event.clientY
     const squareWidth = redSquare.current.offsetWidth
-    if (currColor == "red" && xPos >= redSquare.current.offsetLeft && xPos <= redSquare.current.offsetLeft + squareWidth) {
-      if (yPos >= redSquare.current.offsetTop && yPos <= redSquare.current.offsetTop + squareWidth) {
-        alert('success')
-      }
-    }
-    else if (currColor == "blue" && xPos >= blueSquare.current.offsetLeft && xPos <= blueSquare.current.offsetLeft + squareWidth) {
-      if (yPos >= blueSquare.current.offsetTop && yPos <= blueSquare.current.offsetTop + squareWidth) {
-        alert('success')
-      }
-    }
+    const parentLeft = elements.current.offsetLeft
+    const parentTop = elements.current.offsetTop
 
-    console.log(blueSquare)
-    //if(xPos >= redSquare.width)
-    //If div pos >= 2nd pos && div pos < (2nd pos + 2nd div width)
+    if (currColor == "red" && xPos >= redSquare.current.offsetLeft + parentLeft && xPos <= redSquare.current.offsetLeft + squareWidth + parentLeft) {
+      if (yPos >= redSquare.current.offsetTop + parentTop && yPos <= redSquare.current.offsetTop + squareWidth + parentTop) {
+        event.currentTarget.classList.add("set")
+        event.currentTarget.style.left = `${squareWidth / 2 + redSquare.current.offsetLeft}px`
+      }
+    }
+    else if (currColor == "blue" && xPos >= blueSquare.current.offsetLeft + parentLeft && xPos <= blueSquare.current.offsetLeft + squareWidth + parentLeft) {
+      if (yPos >= blueSquare.current.offsetTop + parentTop && yPos <= blueSquare.current.offsetTop + squareWidth + parentTop) {
+        event.currentTarget.classList.add("set")
+        event.currentTarget.style.left = `${squareWidth / 2 + blueSquare.current.offsetLeft}px`
+      }
+    }
   }
 
   return (
     <div className="content">
-      <div className="elements">
+      <div className="elements" ref={elements}>
         <div className="red square red square" ref={redSquare}></div>
         <div className="blue square" ref={blueSquare}></div>
         <div className="red circle" draggable="true" onMouseDown={shapeColor} onDragEnd={dragItem}></div>
