5a5accc00f54940b130f25099aae5f47e06dee0e
John Doe <local@local.local>
2023-02-21 20:20:43 +0200

    x position fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,14 @@ function App() {
   const redCircle = useRef()
   const blueCircle = useRef()
   const elements = useRef()
+  const [offSetX, setOffsetX] = useState(0)
+  const [offSetY, setOffsetY] = useState(0)
   const [isRedCircleSet, setRedCircle] = useState(false)
   const [isBlueCircleSet, setBlueCircle] = useState(false)
   const [currColor, setCurrColor] = useState("")
 
   const shapeColor = (event: any) => {
-    console.log(event.target.classList)
+    //console.log(event.target.classList)
     event.target.classList.forEach(element => {
       if (element == "red") {
         setCurrColor("red")
@@ -22,16 +24,34 @@ function App() {
         setCurrColor("blue")
       }
     })
+    const xPosMouse = event.clientX
+    const yPosMouse = event.clientY
+    const shapeOffsetX = event.target.offsetLeft
+    const shapeOffsetY = event.target.offsetTop
+    const xOffset = xPosMouse - shapeOffsetX - elements.current.offsetLeft
+    const yOffset = yPosMouse - shapeOffsetY - elements.current.offsetTop
+    console.log(`xOffset: ${xOffset}`)
+    console.log(`yOffset: ${yOffset}`)
+    setOffsetX(xOffset)
+    setOffsetY(yOffset)
+
   }
 
   const dragItem = (event: any) => {
-    const xPos = event.clientX
+    //clientX give the mouse position but it is heavily variable since it depends on the exact position you drag the circle
+    const xPos = event.clientX - offSetX * 2
+    alert(xPos)
     const yPos = event.clientY
+    alert(yPos)
     const squareWidth = redSquare.current.offsetWidth
     const parentLeft = elements.current.offsetLeft
     const parentTop = elements.current.offsetTop
-
+    console.log(`xPos: ${xPos} >= (redSquare.current.offsetLeft + parentLeft): ${redSquare.current.offsetLeft + parentLeft} && xPos: ${xPos} <= (redSquare.current.offsetLeft + squareWidth + parentLeft): ${redSquare.current.offsetLeft + squareWidth + parentLeft}`)
+    console.log(`yPos: ${yPos} >= redSquare.current.offsetTop + parentTop: ${redSquare.current.offsetTop + parentTop} && yPos <= redSquare.current.offsetTop + squareWidth + parentTop: ${redSquare.current.offsetTop + squareWidth + parentTop}`)
+    //alert(`yPos: ${yPos} >= redSquare.current.offsetTop + parentTop: ${redSquare.current.offsetTop + parentTop} && yPos <= redSquare.current.offsetTop + squareWidth + parentTop: ${redSquare.current.offsetTop + squareWidth + parentTop}`)
+    //alert(`xPos: ${xPos} >= (redSquare.current.offsetLeft + parentLeft): ${redSquare.current.offsetLeft + parentLeft} && xPos: ${xPos} <= (redSquare.current.offsetLeft + squareWidth + parentLeft): ${redSquare.current.offsetLeft + squareWidth + parentLeft}`)
     if (currColor == "red" && xPos >= redSquare.current.offsetLeft + parentLeft && xPos <= redSquare.current.offsetLeft + squareWidth + parentLeft) {
+      //alert(`yPos: ${yPos} >= redSquare.current.offsetTop + parentTop: ${redSquare.current.offsetTop + parentTop} && yPos <= redSquare.current.offsetTop + squareWidth + parentTop: ${redSquare.current.offsetTop + squareWidth + parentTop}`)
       if (yPos >= redSquare.current.offsetTop + parentTop && yPos <= redSquare.current.offsetTop + squareWidth + parentTop) {
         event.currentTarget.classList.add("set")
         setRedCircle(true)
@@ -41,8 +61,8 @@ function App() {
         }
       }
     }
-    else if (currColor == "blue" && xPos >= blueSquare.current.offsetLeft + parentLeft && xPos <= blueSquare.current.offsetLeft + squareWidth + parentLeft) {
-      if (yPos >= blueSquare.current.offsetTop + parentTop && yPos <= blueSquare.current.offsetTop + squareWidth + parentTop) {
+    else if (currColor == "blue" && xPos >= blueSquare.current.offsetLeft - 10 + parentLeft && xPos <= blueSquare.current.offsetLeft + squareWidth + parentLeft + 10) {
+      if (yPos >= blueSquare.current.offsetTop + parentTop - 10 && yPos <= blueSquare.current.offsetTop + squareWidth + parentTop + 10) {
         event.currentTarget.classList.add("set")
         setBlueCircle(true)
         event.currentTarget.style.left = `${squareWidth / 2 + blueSquare.current.offsetLeft}px`
