8c593d05bc1d9f8a96434f3c1145a2e8a0446cd6
John Doe <local@local.local>
2023-02-22 17:20:16 +0200

    vite-config-fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "Coding-Task-1-Drag-n-Drop",
-  plugins: [react()],
+  plugins: [react()]
 })
